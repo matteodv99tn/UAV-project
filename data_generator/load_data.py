3b4aab023e6d53286e01c56b29c9aca778b52f4a
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-08-27 18:03:07 +0200

    Repo cleanup

--- data_generator/load_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_pickle("data.pkl")
-
-print(df.head())
-print(type(df.loc[0, "states"]))
--- test.py
+++
@@ -1,7 +0,0 @@
-import yaml
-from yaml.loader import SafeLoader
-
-
-with open('parameters.yaml') as f:
-    data = yaml.load(f, Loader=SafeLoader)
-    print(data)
